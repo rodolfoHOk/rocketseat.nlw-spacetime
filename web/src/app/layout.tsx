37044689e62d3f243b98d734b0e9c235c69c758d
Rodolfo HiOk <rodolfo.okino@gmail.com>
2023-05-24 12:02:48 -0300

    homepage responsive layout

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -32,25 +32,34 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       <body
         className={`${roboto.variable} ${baiJamjurre.variable} bg-gray-900 font-sans text-gray-100`}
       >
-        <main className="grid min-h-screen grid-cols-2">
+        <main className="grid min-h-screen md:grid-cols-2">
           {/* Left */}
-          <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
+          <div className="relative flex flex-col items-start justify-between gap-6 overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-6 py-4 md:px-28 md:py-16">
             {/* Blur */}
             <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full border-r border-white/10 bg-purple-700 opacity-50 blur-full" />
 
             {/* Stripes */}
-            <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
+            <div className="absolute bottom-0 right-2 top-0 hidden w-2 bg-stripes md:block" />
 
             {isAuthenticated ? <Profile /> : <SignIn />}
 
             <Hero />
 
-            <Copyright />
+            <div className="hidden md:block">
+              <Copyright />
+            </div>
           </div>
 
           {/* Right */}
-          <div className="flex max-h-screen flex-col overflow-y-scroll bg-[url(../assets/bg-stars.svg)] bg-cover">
+          <div className="relative flex max-h-screen flex-col bg-[url(../assets/bg-stars.svg)] bg-cover md:overflow-y-scroll">
+            {/* Stripes */}
+            <div className="absolute bottom-0 left-1.5 top-0 w-2 bg-stripes md:hidden" />
+
             {children}
+
+            <div className="flex items-center justify-center pb-4 md:hidden">
+              <Copyright />
+            </div>
           </div>
         </main>
       </body>
